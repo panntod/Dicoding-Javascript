8ec8e2854b8ef735432e5a92005dfa88403c028c
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-11 04:46:50 +0700

    doc: array with looping and if

---
+++ part-5.js
@@ -0,0 +1,19 @@
+/**
+ * TODO:
+ * Buatlah sebuah variabel dengan nama evenNumber yang merupakan sebuah array dengan ketentuan:
+ *   - Array tersebut menampung bilangan genap dari 1 hingga 100
+ *
+ * Catatan:
+ *   - Agar lebih mudah, gunakanlah for loop dan logika if untuk mengisi bilangan genap pada array.
+ */
+
+// Tulis kode di bawah ini
+let evenNumber = [];
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 0) {
+    evenNumber.push(i);
+  }
+  continue;
+}
+
+console.log(evenNumber);
