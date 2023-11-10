3625fafb54e574af3dd0a7571905ea19d920fb2b
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-11 04:40:38 +0700

    doc: variabel and data type

---
+++ part-2.js
@@ -0,0 +1,14 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai "John".
+ *  - lastName: bertipe data string, dengan nilai "Doe".
+ *  - age: bertipe data number, dengan nilai 25.
+ *  - isMarried: bertipe data boolean, dengan nilai true.
+ */
+
+// Tulis kode di bawah ini
+let  firstName = "John"
+let lastName = "Doe"
+let age = 25
+let isMarried = true
