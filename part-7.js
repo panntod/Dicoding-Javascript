99d9aa7aa93d4c9f6005c3847ee07a9245da08e1
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-14 08:32:22 +0700

    [doc] function

---
+++ part-7.js
@@ -0,0 +1,31 @@
+/**
+ * TODO:
+ * 1. Buatlah fungsi bernama minimal dengan ketentuan berikut:
+ *    - Menerima dua buah argumen number, a dan b.
+ *    - Mengembalikan nilai terkecil antara a atau b.
+ *    - Bila nilai keduanya sama, maka kembalikan dengan nilai a
+ *
+ *    contoh:
+ *    minimal(1, 4) // output: 1
+ *    minimal(3, 2) // output: 2
+ *    minimal(3, 3) // output: 3
+ *
+ * 2. Buatlah sebuah function bernama findIndex yang menerima dua parameter, yaitu array dan number.
+ *    Fungsi tersebut harus mengembalikan index dari angka yang sesuai pada array tersebut.
+ *    Jika angka tidak ditemukan, maka kembalikan nilai -1.
+ *
+ *    contoh:
+ *    findIndex([1, 2, 3, 4, 5], 3) // output: 2
+ *    findIndex([1, 2, 3, 4, 5], 6) // output: -1
+ *    findIndex([1, 2, 3, 4, 5], 5) // output: 4
+ */
+
+// Tulis kode di bawah ini
+function minimal(a, b){
+	return a <= b ? a : b
+}
+
+function findIndex(array, number) {
+  const index = array.indexOf(number);
+  return index !== -1 ? index : -1;
+}
